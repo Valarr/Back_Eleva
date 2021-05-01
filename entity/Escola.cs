2bcf40332bf1054414d675bc7518b0234bdc00d3
joao <joaoperdo@gmail.com>
2021-05-01 16:31:19 -0300

    Add project files.

---
+++ entity/Escola.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevaTest.entity
+{
+    public class Escola
+    {
+        public virtual int id_escola { get; set; }
+        public virtual int nome_escola { get; set; }
+        public virtual int telefone { get; set; }
+        public virtual int endereço { get; set; }
+
+
+    }
+}
